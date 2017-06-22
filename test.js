8dabce09c52a4385cc37d342f6122c83e3f0e233
Yoshiya Hinosawa <stibium121@gmail.com>
2017-06-22 10:47:57 +0900

    test: add test

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 const { describe, it } = require('kocha')
+const { execSync } = require('child_process')
+const { existsSync } = require('fs')
+const { join } = require('path')
+const assert = require('assert')
+
+describe('langsheet', () => {
+  it('builds html site', () => {
+    execSync('rm -rf example/build')
+    execSync('node ../index.js build', { cwd: join(__dirname, 'example') })
+    assert(existsSync('./example/build/index.html'))
+  })
+})
