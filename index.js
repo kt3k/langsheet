2d0bdaa2abeb5e95d5fffc21123668445f9f26bc
Yoshiya Hinosawa <stibium121@gmail.com>
2017-12-13 14:55:07 +0900

    feat: make destination configurable

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 
-const { asset, name, on, port, debugPagePath, debugPageTitle, loggerTitle } = require('berber')
+const {
+  asset,
+  name,
+  on,
+  port,
+  dest,
+  debugPagePath,
+  debugPageTitle,
+  loggerTitle
+} = require('berber')
 const pkg = require('./package')
 const pipeline = require('./pipeline')
 
@@ -11,15 +20,21 @@ loggerTitle(pkg.name)
 debugPagePath(`__${pkg.name}__`)
 debugPageTitle(`<i>${pkg.name}</i> debug page`)
 
+const defaultConfig = {
+  port: 26000,
+  dest: 'build'
+}
+
 on('config', config => {
-  config = config || {}
+  config = Object.assign({}, defaultConfig, config)
 
   if (!config.source) {
     console.log(`Error: 'source' is not specified.\nSee ${pkg.homepage} for details.`)
     process.exit(1)
   }
 
-  port(config.port || 26000)
+  port(config.port)
+  dest(config.dest)
 
   pipeline(asset(config.source), config)
 })
